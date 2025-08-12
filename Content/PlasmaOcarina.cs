3b2b29dc090823b98483704f1b16983519db6fcf
TacoConKvass <e_frun@o2.pl>
2025-08-12 20:42:14 +0200

    [Add] Plasma Ocarina

--- Content/PlasmaOcarina.cs
+++ Content/PlasmaOcarina.cs
@@ -1,3 +1,4 @@
+using CalamityMod;
 using CalamityMod.Projectiles.DraedonsArsenal;
 using Microsoft.Xna.Framework;
 using Terraria;
@@ -7,7 +8,6 @@ using Terraria.ModLoader;
 using ThoriumMod;
 using ThoriumMod.Empowerments;
 using ThoriumMod.Items;
-using ThoriumMod.Projectiles.Bard;
 
 namespace BardCompat.Content;
 
@@ -24,7 +24,7 @@ public class PlasmaOcarina : BardItem {
 	}
 
 	public override void SetBardDefaults() {
-		Item.Size = new Vector2(44, 48);
+		Item.Size = new Vector2(38, 31);
 		Item.value = Item.sellPrice(gold: 3);
 
 		Item.useTime = 20;
@@ -39,19 +39,53 @@ public class PlasmaOcarina : BardItem {
 		Item.UseSound = SoundID.Item42;
 		Item.rare = ItemRarityID.LightRed;
 
-		Item.shoot = ProjectileID.PurificationPowder;
+		Item.shoot = ModContent.ProjectileType<PulseRifleShot>();
 		Item.shootSpeed = 10f;
 
 		InspirationCost = 0;
 	}
 
+	int channelValue = 0;
+	int notUsedTimer = 0;
+
 	public override bool AltFunctionUse(Player player) => true;
 
-	public override bool? BardUseItem(Player player) => null;
+	public override void BardHoldItem(Player player) {
+		if (notUsedTimer > 0) notUsedTimer--;
+		if (channelValue > 0 && notUsedTimer <= 0) channelValue = 0;
+	}
 
-	public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) { }
+	public override bool? BardUseItem(Player player) {
+		if (player.altFunctionUse == 2) {
+			notUsedTimer = 10;
+			channelValue++;
+		}
+
+		return player.altFunctionUse == 2 ? channelValue > 180 : null;
+	}
 
 	public override bool BardShoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+		if (channelValue > 180) {
+			Projectile projectile2 = Projectile.NewProjectileDirect(source, player.Center + (HoldoutOffset() ?? Vector2.Zero), Vector2.Zero, ModContent.ProjectileType<WavePounderBoom>(), damage, knockback);
+			float j = 10;
+			projectile2.ai[1] = Main.rand.NextFloat(320f, 870f) + j * 45f;
+			projectile2.localAI[1] = Main.rand.NextFloat(0.08f, 0.25f);
+			projectile2.Opacity = MathHelper.Lerp(0.18f, 0.6f, j / 7f) + Main.rand.NextFloat(-0.08f, 0.08f);
+			projectile2.Calamity().stealthStrike = true;
+			projectile2.DamageType = Item.DamageType;
+			projectile2.netUpdate = true;
+			channelValue = 0;
+			return false;
+		}
+
+		if (player.altFunctionUse != 2) {
+			Projectile shot1 = Projectile.NewProjectileDirect(source, position, velocity.RotatedBy(-.2f), type, damage, knockback);
+			shot1.DamageType = Item.DamageType;
+			shot1.netUpdate = true;
+			Projectile shot2 = Projectile.NewProjectileDirect(source, position, velocity.RotatedBy(.2f), type, damage, knockback);
+			shot2.DamageType = Item.DamageType;
+			shot2.netUpdate = true;
+		}
 		return false;
 	}
 }
